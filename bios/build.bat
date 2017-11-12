beddbca9a8d384cd348f9197cf7a4335a06b3e1d
David Knoll <david@davidknoll.me.uk>
2017-11-12 22:26:27 +0000

    TLIB-ise FatFs, update build process for BIOS and tests

--- bios/build.bat
+++ bios/build.bat
@@ -1,9 +1,11 @@
 cls
+bcc -1 -mc -I..\c -c console.c
+bcc -1 -mc -I..\c -c ddptfdpt.c
 bcc -1 -mc -I..\c -c int08h.c
 bcc -1 -mc -I..\c -c int10h.c
-bcc -1 -mc -I..\c -I..\c\fatfs -c int13h.c
+bcc -1 -mc -I..\c -I..\fatfs -c int13h.c
 bcc -1 -mc -I..\c -c int14h.c
-bcc -1 -mc -I..\c -I..\c\fatfs -c int15h.c
+bcc -1 -mc -I..\c -I..\fatfs -c int15h.c
 bcc -1 -mc -I..\c -c int16h.c
 bcc -1 -mc -I..\c -c int17h.c
 bcc -1 -mc -I..\c -c int18h.c
@@ -16,13 +18,10 @@ bcc -1 -mc -I..\c -DPOLLED -c serial.c
 srec_cat ..\patb\PATB-MTM.com -Binary -Output patb.inc -ASM
 tasm patb.asm, patb.obj
 tasm /d__ROM__ /dINITSP=4000h ..\c\crt0ram.asm, crt0rom.obj
-bcc -1 -mc -I..\c -c console.c
-bcc -1 -mc -I..\c -c ddptfdpt.c
 
-bcc -1 -mc -I..\c -I..\c\fatfs -off.obj -c ..\c\fatfs\ff.c
-bcc -1 -mc -I..\c -I..\c\fatfs -osdmm.obj -c ..\c\fatfs\sdmm.c
-bcc -1 -mc -I..\c -I..\c\fatfs -ounicode.obj -c ..\c\fatfs\ffunicode.c
-bcc -1 -mc -I..\c -I..\c\fatfs -offsystem.obj -c ..\c\fatfs\ffsystem.c
+pushd ..\fatfs
+make fatfs_mc.lib
+popd
 
 tlink @bios.rsp
 rem exe2rom bios.exe bios.bin 7000 7XXX 7C00
